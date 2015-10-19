146c2987e52a264ea3f063a77bfb2a8cf412eec3
Damien Maillard <dmaillard06@gmail.com>
2015-10-19 18:52:44 +0200

    progress on linting

--- linter.py
+++ linter.py
@@ -10,24 +10,26 @@
 
 """This module exports the JsModuleTest plugin class."""
 
+import json
 from SublimeLinter.lint import NodeLinter, util
-
 
 class JsModuleTest(NodeLinter):
 
     """Provides an interface to js-module-test."""
 
     syntax = ('javascript')
-    #executable = 'system-test'
+    executable = 'system-test'
     cmd = 'system-test'
     version_args = '--version'
     version_re = r'(?P<version>\d+\.\d+\.\d+)'
     version_requirement = '>= 0.1.0'
-    regex = r''
-    multiline = False
+    regex = r'^[\s\S]*$'
+    multiline = True
     line_col_base = (1, 1)
-    tempfile_suffix = None
+    tempfile_suffix = '-'
     error_stream = util.STREAM_BOTH
+    # http://www.sublimelinter.com/en/latest/linter_attributes.html#selectors
+    # could use selectors on jshint to auto run this linter when it's a test file
     selectors = {}
     word_re = None
     defaults = {}
@@ -40,6 +42,11 @@ class JsModuleTest(NodeLinter):
 
     def split_match(self, match):
         self.word_re = None
+
+        #with open('data.json', encoding='utf-8') as data_file:
+        #data = json.loads(data_file.read())
+        # I can just read the console output and decide if there is an error or not
+        # the regex should be pretty easy to perform considering I own the output behaviour
 
         if match:
             # match, line, col, error, warning, message, near
